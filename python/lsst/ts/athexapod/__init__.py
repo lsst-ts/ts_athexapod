3dbbf4de77ac3ee693d4866ca3b6139d93159c15
Eric Coughlin <ecoughlin@lsst.org>
2023-07-04 14:43:05 -0400

    Run isort

--- python/lsst/ts/athexapod/__init__.py
+++ python/lsst/ts/athexapod/__init__.py
@@ -3,8 +3,8 @@ try:
 except ImportError:
     __version__ = "?"
 
+from .config_schema import *
 from .controller import *
 from .csc import *
 from .gcserror import *
 from .mock_server import *
-from .config_schema import *
